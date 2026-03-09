b955a1ac93499a6240391ca272a535f99aa8e38a
jy6424 <yjw6424@naver.com>
2026-03-09 17:51:07 +0900

    first commit

---
+++ test_vanilla.c
@@ -0,0 +1,144 @@
+#include "sqlite4.h"
+#include <stdio.h>
+#include <inttypes.h>
+
+static int print_callback(
+  void *NotUsed,
+  int argc,
+  sqlite4_value **argv,
+  const char **colName
+){
+  int i;
+  for (i = 0; i < argc; i++) {
+    const char *val;
+
+    if (argv[i] == 0) {
+      val = "NULL";
+    } else {
+      val = sqlite4_value_text(argv[i], NULL);
+      if (!val) val = "(non-text)";
+    }
+
+    printf("%s=%s\t", colName[i], val);
+  }
+  printf("\n");
+  return 0;
+}
+
+
+int main(int argc, char **argv) {
+  (void)argc; (void)argv;
+
+  sqlite4 *db = 0;
+  int rc = 0;
+
+  rc = sqlite4_open(0, "test_vanilla.db", &db);
+  if (rc) {
+    printf("open failed: %d\n", rc);
+    return 1;
+  }
+  printf("Database opened successfully\n");
+
+  printf("\n-- creating table --\n");
+  rc = sqlite4_exec(
+    db,
+    "CREATE TABLE movies (id INTEGER PRIMARY KEY, dummy BLOB);",
+    0, 0
+  );
+  if (rc) {
+    printf("sql error (create): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+  printf("Table created successfully\n");
+
+  printf("\n-- creating index --\n");
+  rc = sqlite4_exec(
+    db,
+    "CREATE INDEX movies_dummy_idx ON movies (dummy);",
+    0, 0
+  );
+  if (rc) {
+    printf("sql error (create index): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+
+  printf("\n-- created tables --\n");
+  rc = sqlite4_exec(
+    db,
+    "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;",
+    print_callback,
+    0
+  );
+  if (rc) {
+    printf("sql error (select tables): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+
+  printf("\n-- created indexes --\n");
+  rc = sqlite4_exec(
+    db,
+    "SELECT name FROM sqlite_master WHERE type='index' ORDER BY name;",
+    print_callback,
+    0
+  );
+  if (rc) {
+    printf("sql error (select indexes): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+
+  printf("\n-- tables and indexes SQL--\n");
+  rc = sqlite4_exec(
+    db,
+    "SELECT type, name, tbl_name, sql FROM sqlite_master ORDER BY type, name;",
+    print_callback,
+    0
+  );
+  if (rc) {
+    printf("sql error (print tables and indexes): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+
+
+  // insertion
+
+  printf("\n-- inserting data --\n");
+  rc = sqlite4_exec(
+    db,
+    "INSERT INTO movies (dummy) VALUES (X'0123456789ABCDEF');",
+    print_callback,
+    0
+  );
+  if (rc) {
+    printf("sql error (insert): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+  printf("Data inserted successfully\n");
+
+  printf("\n-- selecting data --\n");
+  rc = sqlite4_exec(
+    db,
+    "SELECT dummy FROM movies;",
+    print_callback,
+    0
+  );
+  if (rc) {
+    printf("sql error (select): rc=%d\n", rc);
+    sqlite4_close(db, 0);
+    return 1;
+  }
+  printf("Data selected successfully\n");
+
+  printf("\n-- number of data from table (movies) --\n");
+  sqlite4_exec(db, "SELECT count(*) AS n FROM movies;", print_callback, 0);
+  // printf("\n-- number of data from shadow table (x_idx_shadow) --\n");
+  // sqlite4_exec(db, "SELECT count(*) AS n FROM x_idx_shadow;", print_callback, 0);
+
+  sqlite4_close(db, 0);
+  return 0;
+}
